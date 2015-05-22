c49badb5ec8398da0e6eeb033e91a988a6981023
Baggers <chris.bagley@gmail.com>
2015-05-22 21:59:43 +0200

    progress

--- ceplive-mode.el
+++ ceplive-mode.el
@@ -1,7 +1,7 @@
 ;;- - - - - - - - -
 ;; Cepl Minor Mode
 ;;- - - - - - - - -
-;; Not mandatory but some parts of the coding experience live 
+;; Not mandatory but some parts of the coding experience live
 ;; outside of the language. This is a collection of things
 ;; to make cepl coding as easy (and hopefully fun) as possible.
 
@@ -11,7 +11,7 @@
  (defvar *cepl-snippets-dir* "~/Code/lisp/ceplive-mode/snippets/"))
 
 (defun cepl-test-func ()
-  (interactive)  
+  (interactive)
   (insert "(lambda () (print \"cepl-test\"))"))
 
 (defun ceplive-first-hook ()
@@ -19,28 +19,33 @@
   (yas-activate-extra-mode 'ceplive-mode)
   (ensure-ceplive-loaded))
 
-(defvar eval-into-file-count 0)
 (defun slime-eval-into-file ()
   "Evaluate the current toplevel form.
-store the result in a new global and insert the 
+store the result in a new global and insert the
 var into the code"
   (interactive)
   (let* ((form (slime-defun-at-point))
-         (var-name (concat "<iv-" (number-to-string eval-into-file-count) ">"))
-         (form-with-var (concat "(defparameter " var-name form ")")))
-    (setq eval-into-file-count (+ eval-into-file-count 1))
+         (full-form
+          (concat "(let ((-res- (progn" form ")))
+                       (list (format nil \"~s\" -res-)
+                             (swank::save-presented-object -res-)))")))
+
     (end-of-defun)
-    (slime-eval-async `(swank:eval-and-grab-output ,form-with-var)
+    (slime-eval-async `(swank:eval-and-grab-output ,full-form)
       (lambda (result)
         (cl-destructuring-bind (output value) result
-          (push-mark)
-          (insert value " "))))))
+          (cl-destructuring-bind (str id) (read value)
+            (slime-insert-presentation str id)))))))
+
+
+
+
 
 (defun ensure-ceplive-loaded ()
   "Evaluate the current toplevel form.
-store the result in a new global and insert the 
+store the result in a new global and insert the
 var into the code"
-  (let* ((form "(unless (find-package :ceplive) (print \"loading ceplive-\") (ql:quickload :ceplive))"))    
+  (let* ((form "(unless (find-package :ceplive) (print \"loading ceplive-\") (ql:quickload :ceplive))"))
     (slime-eval-async `(swank:eval-and-grab-output ,form)
       (lambda (result)
         (slime-repl-set-package "ceplive")
